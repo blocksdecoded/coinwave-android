c482bf435efed701a24c18ec9e1fce068e5af7b2
Askar <myrsabekov@gmail.com>
2018-07-23 15:00:50 +0600

    Set fonts

---
+++ app/src/main/kotlin/com/makeuseof/cryptocurrency/util/SFUITypeface.kt
@@ -0,0 +1,35 @@
+package com.makeuseof.cryptocurrency.util
+
+import android.content.Context
+import android.graphics.Typeface
+
+// Created by askar on 5/29/18.
+object SFUITypeface {
+    fun getRegular(context: Context?): Typeface {
+        return Typeface.createFromAsset(context?.assets, "fonts/SFUIDisplay-Regular.otf")
+    }
+
+    fun getSemibold(context: Context?): Typeface {
+        return Typeface.createFromAsset(context?.assets, "fonts/SFUIDisplay-Semibold.otf")
+    }
+
+    fun getBold(context: Context?): Typeface {
+        return Typeface.createFromAsset(context?.assets, "fonts/SFUIDisplay-Bold.otf")
+    }
+
+    fun getHeavy(context: Context?): Typeface {
+        return Typeface.createFromAsset(context?.assets, "fonts/SFUIDisplay-Heavy.otf")
+    }
+
+    fun getMedium(context: Context?): Typeface {
+        return Typeface.createFromAsset(context?.assets, "fonts/SFUIDisplay-Medium.otf")
+    }
+
+    fun getThin(context: Context?): Typeface {
+        return Typeface.createFromAsset(context?.assets, "fonts/SFUIDisplay-Thin.otf")
+    }
+
+    fun getLight(context: Context?): Typeface {
+        return Typeface.createFromAsset(context?.assets, "fonts/SFUIDisplay-Light.otf")
+    }
+}
