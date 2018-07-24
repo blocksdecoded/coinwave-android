e331e8397001eec7c990133ff0541cbaa0e80e02
Askar <myrsabekov@gmail.com>
2018-07-24 14:50:05 +0600

    Add currency activity

---
+++ app/src/main/kotlin/com/makeuseof/cryptocurrency/view/currency/CurrencyActivity.kt
@@ -0,0 +1,8 @@
+package com.makeuseof.cryptocurrency.view.currency
+
+import com.makeuseof.core.SwipeableActivity
+
+// Created by askar on 7/24/18.
+class CurrencyActivity: SwipeableActivity() {
+
+}
