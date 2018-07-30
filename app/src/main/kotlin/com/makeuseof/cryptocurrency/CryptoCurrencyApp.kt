cd43d90267b28d9a868ecef61b9161417bb0e067
Askar <myrsabekov@gmail.com>
2018-07-30 18:30:50 +0600

    Setup fabric

---
+++ app/src/main/kotlin/com/makeuseof/cryptocurrency/CryptoCurrencyApp.kt
@@ -0,0 +1,28 @@
+package com.makeuseof.cryptocurrency
+
+import android.app.Application
+import android.content.Context
+import android.support.multidex.MultiDex
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
+
+// Created by askar on 6/7/18.
+class CryptoCurrencyApp: Application() {
+
+    companion object {
+        var INSTANCE: Application? = null
+    }
+
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+        INSTANCE = this
+        MultiDex.install(this)
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        INSTANCE = this
+
+        Fabric.with(this, Crashlytics())
+    }
+}
