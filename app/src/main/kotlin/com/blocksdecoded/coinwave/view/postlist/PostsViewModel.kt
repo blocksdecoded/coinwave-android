f88f98b1766d4a02b0dca027e81f11dda576566a
Askar <myrsabekov@gmail.com>
2019-02-08 16:51:20 +0600

    Change app name to 'CoinWave'

---
+++ app/src/main/kotlin/com/blocksdecoded/coinwave/view/postlist/PostsViewModel.kt
@@ -0,0 +1,30 @@
+package com.blocksdecoded.coinwave.view.postlist
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.blocksdecoded.coinwave.data.post.model.PublisherPost
+import com.blocksdecoded.core.SingleLiveEvent
+
+/**
+ * Created by askar on 2/8/19
+ * with Android Studio
+ */
+class PostsViewModel: ViewModel() {
+
+    //Data
+    val postsLiveData = MutableLiveData<ArrayList<PublisherPost>>()
+    val loadingLiveData = MutableLiveData<Boolean>()
+
+    //Events
+    val openPostEvent = SingleLiveEvent<String>()
+
+    fun onPostClick(position: Int) {
+        openPostEvent.value = postsLiveData.value?.get(position)?.url
+        openPostEvent.call()
+    }
+
+    fun getNextPosts() {
+
+    }
+
+}
