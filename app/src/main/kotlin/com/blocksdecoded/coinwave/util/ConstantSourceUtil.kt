7e5cf81933dac2e803d6af0e6e38ff7bf94e7082
Askar <myrsabekov@gmail.com>
2019-02-08 17:25:28 +0600

    Update RateUs dialog bg Load coin svg icon

--- app/src/main/kotlin/com/blocksdecoded/coinwave/util/ConstantSourceUtil.kt
+++ app/src/main/kotlin/com/blocksdecoded/coinwave/util/ConstantSourceUtil.kt
@@ -1,9 +1,13 @@
 package com.blocksdecoded.coinwave.util
 
-import android.util.Log
 import android.widget.ImageView
-import com.bumptech.glide.Glide
 import com.blocksdecoded.coinwave.data.model.CurrencyEntity
+import com.blocksdecoded.utils.glide.SvgSoftwareLayerSetter
+import android.graphics.drawable.PictureDrawable
+import com.blocksdecoded.utils.glide.GlideApp
+import com.blocksdecoded.utils.logE
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions.withCrossFade
+
 
 // Created by askar on 7/23/18.
 object ConstantSourceUtil {
@@ -12,11 +16,20 @@ object ConstantSourceUtil {
     }
 }
 
-fun ImageView.loadIcon(currencyEntity: CurrencyEntity){
+fun ImageView.loadIcon(currencyEntity: CurrencyEntity) = try {
 //    loadSvg(currencyEntity.iconUrl)
-    Log.d("ololo", currencyEntity.iconUrl)
-    Glide.with(this)
-            .load(ConstantSourceUtil.getIconUrl(currencyEntity))
-//            .load(currencyEntity.iconUrl)
+
+    GlideApp.with(this)
+            .`as`(PictureDrawable::class.java)
+            .transition(withCrossFade())
+            .listener(SvgSoftwareLayerSetter())
+            .load(currencyEntity.iconUrl)
             .into(this)
+
+//    Glide.with(this)
+//            .load(ConstantSourceUtil.getIconUrl(currencyEntity))
+//            .load(currencyEntity.iconUrl)
+//            .into(this)
+} catch (e: Exception) {
+    logE(e)
 }
