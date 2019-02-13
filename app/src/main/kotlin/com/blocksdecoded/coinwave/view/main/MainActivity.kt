340d9be2c42dcbc63a3e2c54224c141177858b2a
Askar <myrsabekov@gmail.com>
2019-02-13 16:27:31 +0600

    Move layouts strings to strings.xml file Title text sizes to dimens.xml

--- app/src/main/kotlin/com/blocksdecoded/coinwave/view/main/MainActivity.kt
+++ app/src/main/kotlin/com/blocksdecoded/coinwave/view/main/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import android.util.Log
 import androidx.annotation.ColorInt
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.fragment.app.Fragment
@@ -13,7 +12,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
 import com.blocksdecoded.coinwave.R
-import com.blocksdecoded.coinwave.data.ipfs.ServiceExchangeApi
 import com.blocksdecoded.coinwave.domain.UseCaseProvider
 import com.blocksdecoded.coinwave.util.CryptoRateUtil
 import com.blocksdecoded.coinwave.util.SFProTextTypeface
@@ -36,8 +34,6 @@ import com.blocksdecoded.rateus.base.RateUsDialogContract
 import com.blocksdecoded.rateus.base.RateUsListener
 import com.blocksdecoded.utils.*
 import com.blocksdecoded.utils.extensions.setConstraintTopMargin
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.drawer_content.*
 import kotlinx.android.synthetic.main.content_main.*
@@ -209,7 +205,7 @@ class MainActivity :
     }
 
     private fun createCurrencyListScreen(): Fragment =
-            CurrencyListFragment.newInstance(getString(R.string.cryptocurrency)).also {
+            CurrencyListFragment.newInstance(getString(R.string.title_cryptocurrency)).also {
                 mCurrencyListPresenter = CurrencyListPresenter(
                         it,
                         this,
