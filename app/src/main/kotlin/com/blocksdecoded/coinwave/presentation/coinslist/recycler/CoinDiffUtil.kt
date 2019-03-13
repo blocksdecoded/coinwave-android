9e65efe33c9dc433e9ae8040b163f882340dd59a
askar <myrsabekov@gmail.com>
2019-03-13 19:16:17 +0600

    Add exceptions handling to CoinDiffUtil

--- app/src/main/kotlin/com/blocksdecoded/coinwave/presentation/coinslist/recycler/CoinDiffUtil.kt
+++ app/src/main/kotlin/com/blocksdecoded/coinwave/presentation/coinslist/recycler/CoinDiffUtil.kt
@@ -7,19 +7,24 @@ class CoinDiffUtil(
         private val oldCoins: List<CoinEntity>,
         private val newCoins: List<CoinEntity>
 ): DiffUtil.Callback() {
-    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
-            oldCoins[oldItemPosition].id == newCoins[newItemPosition].id
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean = try {
+        oldCoins[oldItemPosition].id == newCoins[newItemPosition].id
+    } catch (e: Exception) {
+        false
+    }
 
     override fun getOldListSize(): Int = oldCoins.size
 
     override fun getNewListSize(): Int = newCoins.size
 
-    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean = try {
         val oldCoin = oldCoins[oldItemPosition]
         val newCoin = newCoins[oldItemPosition]
 
-        return oldCoin.id == newCoin.id && oldCoin.price == newCoin.price
+        oldCoin.id == newCoin.id && oldCoin.price == newCoin.price
                 && oldCoin.priceChange == newCoin.priceChange
                 && oldCoin.marketCap == newCoin.marketCap
+    } catch (e: Exception) {
+        false
     }
 }
